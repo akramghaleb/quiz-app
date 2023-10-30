ebb8bb5f497a74b1d5f006f8eaa8a9956ffadc77
aghammaricme <46090581+akramghaleb@users.noreply.github.com>
2023-10-30 08:36:01 +0300

    map function

--- lib/questions_page.dart
+++ lib/questions_page.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/widgets/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
 
-class Questions extends StatelessWidget {
+class Questions extends StatefulWidget {
   const Questions({super.key});
+
+  @override
+  State<Questions> createState() => _QuestionsState();
+}
+
+class _QuestionsState extends State<Questions> {
+  var currentQuestions = questions[0];
 
   @override
   Widget build(BuildContext context) {
@@ -10,31 +18,22 @@ class Questions extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text(
-            'Question',
-            style: TextStyle(color: Colors.white),
+          Text(
+            currentQuestions.question,
+            style: const TextStyle(color: Colors.white),
           ),
           const SizedBox(
             height: 10,
           ),
-          AnswerButton(
-            text: 'Answer 1',
-            onPressed: () {},
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          AnswerButton(
-            text: 'Answer 2',
-            onPressed: () {},
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          AnswerButton(
-            text: 'Answer 3',
-            onPressed: () {},
-          ),
+          ...currentQuestions.answers.map((e) {
+            return Container(
+              margin: const EdgeInsets.all(5),
+              child: AnswerButton(
+                text: e,
+                onPressed: () {},
+              ),
+            );
+          }),
         ],
       ),
     );
